43c0c30644ac9d18b4fc841902eec7259d40b6f6
Ben Lambert <benlambert43@gmail.com>
2022-06-05 18:45:26 -0400

    add color theme info and app bar

---
+++ ColorTheme.ts
@@ -0,0 +1,14 @@
+const colors = {
+  blue1: '#00B4D8',
+  blue2: '#0096C7',
+  blue3: '#0077B6',
+  blue4: '#023E8A',
+  blue5: '#03045E',
+  orange1: '#FF9E00',
+  orange2: '#FF9100',
+  orange3: '#FF8500',
+  orange4: '#FF6D00',
+  orange5: '#FF5400',
+};
+
+export default colors;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { navigate, useStaticQuery, graphql } from 'gatsby';
 import Button from '@mui/material/Button';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Container,
+  Icon,
+  IconButton,
+  Menu,
+  Toolbar,
+  Tooltip,
+} from '@mui/material';
+import { StaticImage } from 'gatsby-plugin-image';
+import colors from '../../ColorTheme';
 
 type LayoutProps = {
   pageTitle: string;
@@ -23,20 +36,49 @@ const Layout = ({ pageTitle, children }: LayoutProps) => {
       <title>
         {pageTitle} | {data.site.siteMetadata.title}
       </title>
-      <header>{data.site.siteMetadata.title}</header>
+
       <nav>
-        <Button variant="contained">Hello World</Button>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-        </ul>
+        <AppBar position="static" style={{ backgroundColor: colors.blue3 }}>
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Box sx={{ flexGrow: 0 }}>
+                <Avatar>
+                  <StaticImage alt="Wallpaper io logo hero image" src="../images/icon.png" />
+                </Avatar>
+              </Box>
+              <Box sx={{ flexGrow: 1, display: 'flex' }}>
+                <div
+                  style={{
+                    padding: 10,
+                    marginLeft: 30,
+                  }}
+                >
+                  <Button
+                    name="Home"
+                    style={{ paddingLeft: 20, paddingRight: 20, color: 'white' }}
+                    onClick={() => {
+                      navigate('/');
+                    }}
+                  >
+                    Home
+                  </Button>
+                </div>
+                <div
+                  style={{
+                    padding: 10,
+                  }}
+                >
+                  <Button
+                    name="About"
+                    style={{ paddingLeft: 20, paddingRight: 20, color: 'white' }}
+                  >
+                    About
+                  </Button>
+                </div>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
       </nav>
       <main>
         <h1>{pageTitle}</h1>
