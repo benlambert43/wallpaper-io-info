ec9131cbea538672b006f1265f423220f1e82c71
Ben Lambert <benlambert43@gmail.com>
2022-06-06 16:12:44 -0400

    add email support page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,42 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import { StaticImage } from 'gatsby-plugin-image';
-import { Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import colors from '../../ColorTheme';
 
 const ContactPage = () => {
+  const [email, setEmail] = React.useState<string>();
+
+  const contactStr1 = 'benlambertdev';
+  const contactStr2 = '43';
+  const contactStr3 = '@';
+  const contactStr4 = 'gmail';
+  const contactStr5 = '.com';
+
+  const generateStr = () => {
+    return contactStr1 + contactStr2 + contactStr3 + contactStr4 + contactStr5;
+  };
+
   return (
     <Layout pageTitle='Contact Us'>
-      <Typography>This is my contact page.</Typography>
+      <Typography variant='h6'>To generate contact email, press the button below.</Typography>
+      <Typography color={colors.gray3} variant='subtitle2' style={{ paddingTop: 10 }}>
+        (helps prevent bots)
+      </Typography>
+      <Box style={{ paddingTop: 20, paddingBottom: 50 }}>
+        {email ? (
+          email
+        ) : (
+          <Button
+            onClick={() => {
+              setEmail(generateStr());
+            }}
+            variant='contained'
+          >
+            Generate support email
+          </Button>
+        )}
+      </Box>
     </Layout>
   );
 };
