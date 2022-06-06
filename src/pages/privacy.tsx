4fbd1ef0095b7c73b3007abd7ecf6b3027b7cc96
Ben Lambert <benlambert43@gmail.com>
2022-06-06 16:46:24 -0400

    privacy policy is done

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import { StaticImage } from 'gatsby-plugin-image';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { HTMLPrivacyPol } from '../privacypol';
 
 const PrivacyPage = () => {
+  function privacyIframe() {
+    return {
+      __html: HTMLPrivacyPol,
+    };
+  }
+
   return (
-    <Layout pageTitle="Privacy Policy">
-      <Typography>This is my Privacy Policy Page.</Typography>
+    <Layout pageTitle='Privacy Policy'>
+      <Box>
+        <div style={{ paddingTop: 20, paddingBottom: 20 }}>
+          <hr />
+        </div>
+        <div dangerouslySetInnerHTML={privacyIframe()} />
+      </Box>
     </Layout>
   );
 };
